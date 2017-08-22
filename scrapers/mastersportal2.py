15499607add035952a9602d4dd3e03aad57301d8
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-08-22 20:02:57 +0530

    Bitch please

--- scrapers/mastersportal2.py
+++ scrapers/mastersportal2.py
@@ -4,14 +4,6 @@ import json
 import pprint
 import urllib.request
 
-def get_links(filename):
-    #TODO:replace filename with URL and use a request
-    with open(filename) as data_file:    
-        data = json.load(data_file)
-        pprint.pprint(data)
-    return
-
-# get_links('../mp/data.json')
 
 def download_jsons(url):
     global count
@@ -24,8 +16,30 @@ def download_jsons(url):
                 json.dump(data,out)
     except Exception as e:
         print(e)
+
+
 def download_json_driver():
     count = 0
     url = "https://reflector.prtl.co/?order=relevance&direction=desc&start=0&include_order=true&token=ddd2a935ef77852e1a7b108eddeddc7b7f598a80&q=de-fulltime%7Cdi-101%7Cen-3605%7Clv-master%2Cpreparation%7Cmh-face2face%7C!dg-prebachelor%2Clanguage%7Cuc-108&path=data%2Fstudies-cs%2Fpublic%2Fresults%2F&hh=en-GB%2Cde-DE%2Cfr-FR%2Ces-ES-master%2Cdefault" 
     [download_jsons(url.replace("start=0","start="+repr(x))) for x in range(0,500,10)]
-	
+
+
+def handle_one_course(indict,id):
+    course = {}
+    pprint.pprint(indict)
+    if "virtual_path" in indict:
+        course["CourseURL"] = "http://www.mastersportal.eu/studies/"+id+"/"+indict["virtual_path"]
+    if "tuition_fee_types" in indict:
+        
+
+
+
+
+def decode_json(filename):
+    with open(filename) as data_file:    
+        data = json.load(data_file)
+        # for x in data:
+        #     handle_one_course(data[x],x)
+        handle_one_course(data["16725"],"16725")
+    return
+decode_json("../mp/response1.json")
