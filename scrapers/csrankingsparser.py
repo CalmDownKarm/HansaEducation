44cd37157806cd3fe6945ee850d5569386fa1feb
CalmDownKarm <karmanya.aggarwal.14cse@bml.edu.in>
2017-09-10 14:48:39 +0530

    CSRankings script done

--- scrapers/csrankingsparser.py
+++ scrapers/csrankingsparser.py
@@ -7,10 +7,30 @@ from os import listdir
 from os.path import isfile, join
 
 def extract_table_data(file):
+    list_of_schools = []
     with open(file) as f:
-        soup = bs(f.read,"html.parser")
+        soup = bs(f.read(),"html.parser")
         if soup:
-            pass
+            rows = soup.find_all("tr")
+            for row in rows:
+                tds = row.find_all("td")
+                try:
+                    if(len(tds)==4):
+                        school = {}
+                        school["Rank"] = tds[0].get_text().strip()
+                        school["Name"] = tds[1].get_text().strip()[1:-2]
+                        school["Avg Publication Count"] = tds[2].get_text().strip()
+                        school["Num. Published Faculty"] = tds[3].get_text().strip()
+                        if(school["Rank"]):
+                            pprint.pprint(school)
+                            list_of_schools.append(school)
+                except IndexError:
+                    print("skipped one")
         else:
             print("No soup for you")
-        
+    return list_of_schools
+
+files = [join("../CSRankings/", f) for f in listdir("../CSRankings/") if isfile(join("../CSRankings", f))]
+data = extract_table_data(files[0])
+df = pd.DataFrame(data)
+df.to_csv("../Output/CSRankingsCSV.csv")
